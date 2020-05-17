76078267ce9feac83325c4002b656a7fccf0c781
Matt Cestarte <matthew.cestarte@runbox.com>
2020-05-17 13:59:41 -0500

    get user input, need to match on <digitX>,<digitY>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::fmt;
+use std::io;
+//use std::io::prelude::*;
 
 const BOARD_WIDTH: usize = 3;
 const BOARD_HEIGHT: usize = 3;
@@ -30,6 +32,7 @@ struct Tile {
     row: u8,
     col: u8,
     state: CellState,
+    active_player: u8,
 }
 
 impl Game {
@@ -38,7 +41,8 @@ impl Game {
             board: [[Tile{
                 row: 0,
                 col: 0,
-                state: CellState::Empty
+                state: CellState::Empty,
+                active_player: 1,
             }; BOARD_WIDTH]; BOARD_HEIGHT]
         };
 
@@ -56,7 +60,7 @@ impl Game {
             }
 
             for x in 0..BOARD_WIDTH {
-                if (x == 0) {
+                if x == 0 {
                     print!("{}  ", y+1);
                 }
                 print!("{}  ", self.board[y][x].state);
@@ -65,26 +69,39 @@ impl Game {
         }
     }
 
-    // fn isOver(&self) -> bool {
-    //     // TODO check for winner
-    //     let filtered = self.board.filter(|&filteredTiles| filteredTiles.CellState == CellState::O);
-    //         false
+    fn get_input(&self) {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();//.expect("Failed to read user input");
+        println!("input was \"{}\"", input.trim_end());
+        match input.as_str() {
+            "X" => println!("X"),
+            "O" => println!("O"),
+            _ => println!("unknown"),
+        }
+    }
+
+    fn is_over(&self) -> bool {
+        // TODO check for winner
+        //let filtered = self.board.filter(|&filteredTiles| filteredTiles.CellState == CellState::O);
+        //    false
         
-    //     true
-    // }
+        true
+    }
 }
 
 
 fn main() {
     let game = Game::new();
-    game.print();
+    //game.print();
 
-    // let mut done = false;
-    // while !done {
-    //     game.print();
+    let mut done = false;
+    while !done {
+        game.print();
+        game.get_input();
 
-    //     if (game.isOver())
-    //         done = true;
-    // }
+        if game.is_over() {
+            done = true;
+        }
+    }
     
 }
